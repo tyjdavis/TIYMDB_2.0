1df3e279df00ebae7af3671ae2f120487e18d36a
Chris Lebbano <chris.lebbano@gmail.com>
2017-04-21 15:55:00 -0400

    fixed bug by forcing async off , sorry

--- public/jQueryIndex.js
+++ public/jQueryIndex.js
@@ -1,39 +1,57 @@
 let arrayOfMovies = [];
 
 function main(){
-  getData().then(getRuntimes).then(getCasts).then(getYTs).then(displayMovies);
+  getData();
+  getRuntimes();
+  getCasts();
+  getYTs();
+  displayMovies();
 }
+main();
 
 function getData(){
-  return $.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${keys.moviedb}&language=en-US&page=1`, function(data){
-    arrayOfMovies = data.results.map((obj)=>{
-      let movie = new Movie();
-      movie.poster_path = obj.poster_path;
-      movie.overview = obj.overview;
-      movie.release_date = obj.release_date;
-      movie.title = obj.title;
-      movie.vote_average = obj.vote_average;
-      movie.id = obj.id;
-      return movie;
-    });
+  let movieurl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${keys.moviedb}&language=en-US&page=1`;
+  $.ajax({
+    url: movieurl,
+    async: false,
+    success: function(data){
+      arrayOfMovies = data.results.map((obj)=>{
+        let movie = new Movie();
+        movie.poster_path = obj.poster_path;
+        movie.overview = obj.overview;
+        movie.release_date = obj.release_date;
+        movie.title = obj.title;
+        movie.vote_average = obj.vote_average;
+        movie.id = obj.id;
+        return movie;
+      });
+    }
   });
 }
 
 function getRuntimes(){
-  return arrayOfMovies.map((movie)=>{
-    return $.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${keys.moviedb}&language=en-US`, function(data){
-      movie.runtime = data.runtime;
+  arrayOfMovies.forEach((movie)=>{
+    $.ajax({
+      url: `https://api.themoviedb.org/3/movie/${movie.id}?api_key=${keys.moviedb}&language=en-US`,
+      async: false,
+      success: function(data){
+        movie.runtime = data.runtime;
+      }
     });
   });
 }
 
 function getCasts(){
-  return arrayOfMovies.map((movie)=>{
-    return $.get(`https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${keys.moviedb}`, function(data) {
-      movie.cast = data.cast.slice(0, 3);
-      movie.cast = movie.cast.map((obj)=> obj.name);
-      movie.cast = movie.cast.join(', ');
-      movie.crew = data.crew[0].name;
+  arrayOfMovies.forEach((movie)=>{
+    $.ajax({
+      url: `https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${keys.moviedb}`,
+      async: false,
+      success: function(data){
+        movie.cast = data.cast.slice(0, 3);
+        movie.cast = movie.cast.map((obj)=> obj.name);
+        movie.cast = movie.cast.join(', ');
+        movie.crew = data.crew[0].name;
+      }
     });
   });
 }
@@ -51,5 +69,3 @@ function displayMovies(){
     movie.display();
   });
 }
-
-main();
