aaf63e5e9ddc7f39e11ac9ccaa4fca2bce5da3e9
Chris Lebbano <chris.lebbano@gmail.com>
2017-04-21 15:32:47 -0400

    Cant get rid of bug

--- public/jQueryIndex.js
+++ public/jQueryIndex.js
@@ -1,12 +1,12 @@
 let arrayOfMovies = [];
 
 function main(){
-  getMovies().then(displayMovies);
+  getData().then(getRuntimes).then(getCasts).then(getYTs).then(displayMovies);
 }
 
-function getMovies(){
+function getData(){
   return $.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${keys.moviedb}&language=en-US&page=1`, function(data){
-    arrayOfMovies =  data.results.map((obj)=>{
+    arrayOfMovies = data.results.map((obj)=>{
       let movie = new Movie();
       movie.poster_path = obj.poster_path;
       movie.overview = obj.overview;
@@ -14,32 +14,35 @@ function getMovies(){
       movie.title = obj.title;
       movie.vote_average = obj.vote_average;
       movie.id = obj.id;
-      getRuntime(movie);
-      getCast(movie);
-      getYT(movie);
       return movie;
     });
   });
 }
 
-function getRuntime(movie){
-  return $.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${keys.moviedb}&language=en-US`, function(data){
-    movie.runtime = data.runtime;
+function getRuntimes(){
+  return arrayOfMovies.map((movie)=>{
+    return $.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${keys.moviedb}&language=en-US`, function(data){
+      movie.runtime = data.runtime;
+    });
   });
 }
 
-function getCast(movie){
-  return $.get(`https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${keys.moviedb}`, function(data) {
-    movie.cast = data.cast.slice(0, 3);
-    movie.cast = movie.cast.map((obj)=> obj.name);
-    movie.cast = movie.cast.join(', ');
-    movie.crew = data.crew[0].name;
+function getCasts(){
+  return arrayOfMovies.map((movie)=>{
+    return $.get(`https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${keys.moviedb}`, function(data) {
+      movie.cast = data.cast.slice(0, 3);
+      movie.cast = movie.cast.map((obj)=> obj.name);
+      movie.cast = movie.cast.join(', ');
+      movie.crew = data.crew[0].name;
+    });
   });
 }
 
-function getYT(movie){
-  return $.get(`https://www.googleapis.com/youtube/v3/search?key=${keys.google}&part=snippet&q=${movie.title}}trailer`, function(data){
-    movie.YTID = data.items[0].id.videoId;
+function getYTs(){
+  return arrayOfMovies.map((movie)=>{
+    return $.get(`https://www.googleapis.com/youtube/v3/search?key=${keys.google}&part=snippet&q=${movie.title}}trailer`, function(data){
+      movie.YTID = data.items[0].id.videoId;
+    });
   });
 }
 
