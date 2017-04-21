89faa024062e466eb8384cef51719cc1c701364b
Tyler Davis <tyjdavis@outlook.com>
2017-04-21 15:36:28 -0400

    changes

--- public/yt_api.js
+++ public/yt_api.js
@@ -7,11 +7,8 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
-<<<<<<< HEAD
-    height: '680',
-=======
+
     height: '600',
->>>>>>> aaf63e5e9ddc7f39e11ac9ccaa4fca2bce5da3e9
     width: '900',
     videoId: 'M7lc1UVf-VE',
     events: {
